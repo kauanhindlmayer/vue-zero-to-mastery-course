d46a805af866a4f353e04c1a2ad800e6366424b3
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-01-04 18:30:28 -0300

    lecture: reactive references

---
+++ section-21/reactive-references/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click.prevent="increment">Click me</button>
+    <p>{{ num }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+
+    // console.log(num);
+
+    function increment() {
+      num.value++;
+    }
+
+    return {
+      num,
+      increment,
+    };
+  },
+};
+</script>
