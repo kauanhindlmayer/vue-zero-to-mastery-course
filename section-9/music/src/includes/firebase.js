0bd57ab632de745e761b21d378e7028e5e4fb5ae
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-01-10 21:20:08 -0300

    lecture: setting up the firebase sdk

---
+++ section-9/music/src/includes/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUiKjJHf2aDmF9PZDE82r6BMVuMC4ID5A",
+  authDomain: "music-4ddea.firebaseapp.com",
+  projectId: "music-4ddea",
+  storageBucket: "music-4ddea.appspot.com",
+  appId: "1:11970667896:web:88a6c3253d689fbba7d48d",
+};
+
+export default firebase.initializeApp(firebaseConfig);
