318b8c18dce9b0651c831d459d676dfc4e8cabf5
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-01-11 21:28:22 -0300

    lecture: exporting services

--- section-9/music/src/includes/firebase.js
+++ section-9/music/src/includes/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAUiKjJHf2aDmF9PZDE82r6BMVuMC4ID5A",
@@ -9,4 +10,10 @@ const firebaseConfig = {
   appId: "1:11970667896:web:88a6c3253d689fbba7d48d",
 };
 
-export default firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export { auth, db };
