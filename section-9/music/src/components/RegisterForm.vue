fdd93b6942f2d4e667b561fb91e8f738f3a09075
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-01-11 21:43:43 -0300

    lecture: storing the user data

--- section-9/music/src/components/RegisterForm.vue
+++ section-9/music/src/components/RegisterForm.vue
@@ -107,7 +107,7 @@
 </template>
 
 <script>
-import { auth } from "@/includes/firebase";
+import { auth, usersCollection } from "@/includes/firebase";
 
 export default {
   name: "RegisterForm",
@@ -146,6 +146,22 @@ export default {
           values.password
         );
       } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = "bg-red-500";
+        this.reg_alert_msg =
+          "An unexpected error ocurred. Please try again later.";
+        return;
+      }
+
+      try {
+        await usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country,
+        });
+      } catch (error) {
+        console.log(error)
         this.reg_in_submission = false;
         this.reg_alert_variant = "bg-red-500";
         this.reg_alert_msg =
