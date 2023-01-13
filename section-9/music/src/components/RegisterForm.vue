9c76f78b035e1e649dccb07b95a123f3001d7f5a
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-01-12 21:54:58 -0300

    lecture: loggin the user in after signin

--- section-9/music/src/components/RegisterForm.vue
+++ section-9/music/src/components/RegisterForm.vue
@@ -108,6 +108,8 @@
 
 <script>
 import { auth, usersCollection } from "@/includes/firebase";
+import { mapWritableState } from "pinia";
+import useUserStore from "../stores/user";
 
 export default {
   name: "RegisterForm",
@@ -130,6 +132,9 @@ export default {
       reg_alert_variant: "bg-blue-500",
       reg_alert_msg: "Please wait! Your account is being created.",
     };
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
   },
   methods: {
     async register(values) {
@@ -161,13 +166,15 @@ export default {
           country: values.country,
         });
       } catch (error) {
-        console.log(error)
+        console.log(error);
         this.reg_in_submission = false;
         this.reg_alert_variant = "bg-red-500";
         this.reg_alert_msg =
           "An unexpected error ocurred. Please try again later.";
         return;
       }
+
+      this.userLoggedIn = true;
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
