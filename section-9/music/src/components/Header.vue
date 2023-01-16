8abb928d92d217cf0bc6a47bdcb2a684a6fbdc47
Kauan <kauanhindlmayer07@gmail.com>
2023-01-16 08:41:48 -0300

    lecture: custom links

--- section-9/music/src/components/Header.vue
+++ section-9/music/src/components/Header.vue
@@ -3,10 +3,7 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link
-        class="text-white font-bold uppercase text-2xl mr-4"
-        to="/"
-      >
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/">
         Music
       </router-link>
 
@@ -14,6 +11,9 @@
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
+          </li>
           <li v-if="!userStore.userLoggedIn">
             <a
               class="px-2 text-white"
@@ -25,7 +25,9 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" to="/manage">
+                Manage
+              </router-link>
             </li>
             <li>
               <a
