302eaaacc5ff238ed57f65da53eb7022f5a55622
Kauan <kauanhindlmayer07@gmail.com>
2023-01-17 11:23:20 -0300

    lecture: improving the progress bar

--- section-9/music/src/components/Upload.vue
+++ section-9/music/src/components/Upload.vue
@@ -23,10 +23,17 @@
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">{{ upload.name }}</div>
+        <div class="font-bold text-sm" :class="upload.text_class">
+          <i :class="upload.icon"></i>
+          {{ upload.name }}
+        </div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
-          <div class="transition-all progress-bar bg-blue-400" :class="'bg-blue-400'" :style="{ width: upload.current_progress + '%' }"></div>
+          <div
+            class="transition-all progress-bar"
+            :class="upload.variant"
+            :style="{ width: upload.current_progress + '%' }"
+          ></div>
         </div>
       </div>
     </div>
@@ -56,15 +63,21 @@ export default {
         const songsRef = storageRef.child(`songs/${file.name}`); // music-4ddea.appspot.com/songs/example.mp3
         const task = songsRef.put(file);
 
-        this.uploads.push({
-          task,
-          current_progress: 0,
-          name: file.name,
-        });
+        const uploadIndex =
+          this.uploads.push({
+            task,
+            current_progress: 0,
+            name: file.name,
+            variant: "bg-blue-400",
+            icon: "fas fa-spinner fa-spin",
+            text_class: "",
+          }) - 1;
 
         task.on("state_changed", (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+          this.uploads[uploadIndex].current_progress = progress;
         });
       });
 
