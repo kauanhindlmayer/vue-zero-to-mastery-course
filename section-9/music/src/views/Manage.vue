919fb4271ad868b4a97508e70d141c2f51edf3bd
Kauan <kauanhindlmayer07@gmail.com>
2023-01-18 10:54:09 -0300

    lecture: querying the database

--- section-9/music/src/views/Manage.vue
+++ section-9/music/src/views/Manage.vue
@@ -134,11 +134,17 @@
 <script>
 // import useUserStore from "../stores/user";
 import AppUpload from "../components/Upload.vue";
+import { songsCollection, auth } from "../includes/firebase";
 
 export default {
   name: "manage",
   components: {
     AppUpload,
+  },
+  async created() {
+    const snapshot = await songsCollection
+      .where("uid", "==", auth.currentUser.uid)
+      .get();
   },
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
