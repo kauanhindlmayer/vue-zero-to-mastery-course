3716536f888a1bfdf671cd2503c9947d3ace6c62
Kauan <kauanhindlmayer07@gmail.com>
2023-01-19 08:44:10 -0300

    lecture: updating the list of songs after an upload

--- section-9/music/src/components/Upload.vue
+++ section-9/music/src/components/Upload.vue
@@ -45,6 +45,7 @@
 import { storage, auth, songsCollection } from "../includes/firebase";
 export default {
   name: "Upload",
+  props: ["addSong"],
   data() {
     return {
       is_dragover: false,
@@ -101,7 +102,10 @@ export default {
             };
 
             song.url = await task.snapshot.ref.getDownloadURL();
-            await songsCollection.add(song);
+            const songRef = await songsCollection.add(song);
+            const songSnapshot = await songRef.get();
+
+            this.addSong(songSnapshot);
 
             this.uploads[uploadIndex].variant = "bg-green-400";
             this.uploads[uploadIndex].icon = "fas fa-check";
