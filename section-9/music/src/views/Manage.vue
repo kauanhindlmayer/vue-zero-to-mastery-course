a5ec421d9d7efb589bbda2eddeeb7a254f34ab21
Kauan <kauanhindlmayer07@gmail.com>
2023-01-19 08:59:16 -0300

    lecture: router leave guards

--- section-9/music/src/views/Manage.vue
+++ section-9/music/src/views/Manage.vue
@@ -24,6 +24,7 @@
               :updateSong="updateSong"
               :index="i"
               :removeSong="removeSong"
+              :updateUnsavedFlag="updateUnsavedFlag"
             />
           </div>
         </div>
@@ -47,6 +48,7 @@ export default {
   data() {
     return {
       songs: [],
+      unsavedFlag: false,
     };
   },
   async created() {
@@ -72,6 +74,19 @@ export default {
 
       this.songs.push(song);
     },
+    updateUnsavedFlag(value) {
+      this.unsavedFlag = value;
+    },
+  },
+  beforeRouteLeave(to, from, next) {
+    if (!this.unsavedFlag) {
+      next();
+    } else {
+      const leave = confirm(
+        "You have unsaved changes. Are you sure you want to leave?"
+      );
+      next(leave);
+    }
   },
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
