4174cb4b9cfa3ffc36eb3db3f74dccaca1068b18
Kauan <kauanhindlmayer07@gmail.com>
2023-01-19 08:21:01 -0300

    lecture: editing a song

--- section-9/music/src/views/Manage.vue
+++ section-9/music/src/views/Manage.vue
@@ -17,7 +17,13 @@
           </div>
           <div class="p-6">
             <!-- Composition Items -->
-            <composition-item v-for="song in songs" :key="song.docID" :song="song" />
+            <composition-item
+              v-for="(song, i) in songs"
+              :key="song.docID"
+              :song="song"
+              :updateSong="updateSong"
+              :index="i"
+            />
           </div>
         </div>
       </div>
@@ -28,7 +34,7 @@
 <script>
 // import useUserStore from "../stores/user";
 import AppUpload from "../components/Upload.vue";
-import CompositionItem from '../components/CompositionItem.vue';
+import CompositionItem from "../components/CompositionItem.vue";
 import { songsCollection, auth } from "../includes/firebase";
 
 export default {
@@ -55,6 +61,12 @@ export default {
 
       this.songs.push(song);
     });
+  },
+  methods: {
+    updateSong(i, values) {
+      this.songs[i].modified_name = values.modified_name;
+      this.songs[i].genre = values.genre;
+    },
   },
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
