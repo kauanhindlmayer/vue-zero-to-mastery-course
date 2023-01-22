beca2286dbdc2b89fbda0f4d764d5b3782909d23
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-01-22 00:06:49 -0300

    lecture: updating the comments list

--- section-9/music/src/views/Song.vue
+++ section-9/music/src/views/Song.vue
@@ -59,6 +59,7 @@
         <!-- Sort Comments -->
         <select
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          v-model="sort"
         >
           <option value="1">Latest</option>
           <option value="2">Oldest</option>
@@ -70,7 +71,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <!-- Comment Author -->
@@ -108,10 +109,20 @@ export default {
       comment_alert_variant: "bg-blue-500",
       comment_alert_message: "Please wait! Your comment is being submitted",
       comments: [],
+      sort: 1,
     };
   },
   computed: {
     ...mapState(useUserStore, ["userLoggedIn"]),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === 1) {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   methods: {
     async addComment(values, { resetForm }) {
@@ -130,6 +141,8 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.getComments();
 
       this.comment_in_submission = false;
       this.comment_alert_variant = "bg-green-500";
