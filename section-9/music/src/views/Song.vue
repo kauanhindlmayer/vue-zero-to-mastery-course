399ab35b1225a2835fab48a657c2f623a8967b1a
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-01-22 10:37:13 -0300

    lecture: detecting query parameters

--- section-9/music/src/views/Song.vue
+++ section-9/music/src/views/Song.vue
@@ -173,11 +173,16 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+
+    this.sort = sort === "1" || sort === "2" ? sort : "1";
+
     this.song = docSnapshot.data();
     this.getComments();
   },
   watch: {
     sort(newValue) {
+      if (newValue === this.$route.query.sort) return;
       this.$router.push({ query: { sort: newValue } });
     },
   },
