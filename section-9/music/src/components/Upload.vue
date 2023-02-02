5bde6f70d34f0c3012f0710d2d084f0b09f93cec
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-02-01 21:47:19 -0300

    lecture: handling offline uploads

--- section-9/music/src/components/Upload.vue
+++ section-9/music/src/components/Upload.vue
@@ -63,6 +63,18 @@ export default {
       files.forEach((file) => {
         if (file.type !== "audio/mpeg") return;
 
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: "bg-red-400",
+            icon: "fas fa-times",
+            text_class: "text-red-400",
+          });
+          return;
+        }
+
         const storageRef = storage.ref(); // music-4ddea.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`); // music-4ddea.appspot.com/songs/example.mp3
         const task = songsRef.put(file);
