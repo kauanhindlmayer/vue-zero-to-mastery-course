69b1ccd98c987331a5ab53234d9fc881c8e25171
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-02-09 22:13:07 -0300

    lecture: mocking methods

--- section-9/music/src/components/__tests__/home.spec.js
+++ section-9/music/src/components/__tests__/home.spec.js
@@ -11,10 +11,19 @@ describe("Home.vue", () => {
           songs,
         };
       },
+      global: {
+        mocks: {
+          $t: (message) => message,
+        },
+      },
     });
 
     const items = component.findAllComponents(SongItem);
 
     expect(items).toHaveLength(songs.length);
+
+    items.forEach((wrapper, i) => {
+      expect(wrapper.props().song).toStrictEqual(songs[i]);
+    });
   });
 });
