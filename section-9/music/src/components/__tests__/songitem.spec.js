0dab75defdbaa8f43528892627cb541a06fb08a9
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-02-10 22:18:30 -0300

    lecture: testing attributes

--- section-9/music/src/components/__tests__/songitem.spec.js
+++ section-9/music/src/components/__tests__/songitem.spec.js
@@ -22,4 +22,27 @@ describe("SongItem.vue", () => {
 
     expect(compositionAuthor.text().toBe(song.display_name));
   });
+
+  test("render song.docID in id attribute", () => {
+    const song = {
+      docID: "abc",
+    };
+
+    const wrapper = shallowMount(SongItem, {
+      propsData: {
+        song,
+      },
+      global: {
+        components: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+
+    const compositionAuthor = wrapper.find(".text-gray-500");
+
+    // expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`);
+
+    expect(wrapper.classes()).toContain(`song-id-${song.docID}`);
+  });
 });
