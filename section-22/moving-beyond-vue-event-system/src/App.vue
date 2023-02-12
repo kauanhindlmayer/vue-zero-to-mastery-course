bafba4b51a44458f6044937ee5cc4e13af37caec
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-02-12 20:10:37 -0300

    lecture: encapsulating scrolling

--- section-22/moving-beyond-vue-event-system/src/App.vue
+++ section-22/moving-beyond-vue-event-system/src/App.vue
@@ -1,10 +1,116 @@
 <template>
   <div id="app">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
+
     <div class="mt-2 ml-2">
       <button type="button" class="btn btn-primary" @click="open = true">
         Open
       </button>
     </div>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed nibh
+      a leo elementum gravida. Sed felis purus, maximus in euismod a, laoreet in
+      justo. Proin tincidunt nisi lacus, eu pretium urna interdum posuere. In
+      sem leo, sagittis nec ante vitae, fringilla efficitur velit. Proin ante
+      ante, venenatis molestie enim vel, varius sagittis magna. Integer erat
+      lectus, laoreet ac erat et, tristique sagittis sem. Pellentesque in nisl
+      eros. Donec tincidunt, ipsum sed mollis accumsan, dui sem aliquet est,
+      venenatis bibendum purus lectus id nulla. Aliquam commodo tortor in arcu
+      dignissim, in tempus lectus dictum. Morbi mollis mauris in felis ultrices,
+      sit amet mollis lacus aliquam. Donec sit amet quam odio. In aliquam nisl
+      velit, ut blandit mauris volutpat id.
+    </p>
 
     <app-modal :show="open" @hide="open = false" />
   </div>
@@ -20,7 +126,7 @@ export default {
   },
   data() {
     return {
-      open: false,
+      open: true,
     };
   },
 };
